8be07cea260a2609d5d8e05c94de5fbc8874657a
acruz-23 <cruz.581@outlook.com>
2023-05-01 10:30:30 -0400

    added a script that makes and calls basic functions.

---
+++ simple_functions.js
@@ -0,0 +1,23 @@
+function favoriteThings() {
+  console.log("Name: Adan Cruz");
+  console.log("Favorite Movie: John Wick Chapter 4");
+  console.log("Favorite band: In Flames");
+}
+
+function whyImHere() {
+  console.log(
+    "I am here to improve my programing skills and learn more about the software development process from a real company. Getting a job would be nice too."
+  );
+}
+
+function favortiePlace() {
+  console.log(
+    "Favorite Place: There is a spot on pigeon hill that I like to go to. It requires a bit of a hike and is just off trail. The spot has a lot of rocks to sit on, a serene atmosphere, and is generally a good place to read."
+  );
+}
+
+favoriteThings();
+
+whyImHere();
+
+favortiePlace();
